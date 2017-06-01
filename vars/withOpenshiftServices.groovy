b2b71a2bdcd2801834adb787d241e931d993829f
Gerard Ryan <gerard@ryan.lt>
2017-06-01 09:59:30 +0100

    ✨ Add MySQL to withOpenshiftServices

--- vars/withOpenshiftServices.groovy
+++ vars/withOpenshiftServices.groovy
@@ -32,7 +32,7 @@ def call(services, body) {
 }
 
 String sanitizeObjectName(s) {
-    s.replace('_', '-').toLowerCase()
+    s.replaceAll(/[_ ]/, '-').toLowerCase()
 }
 
 Map<String, String> getNames(services) {
@@ -47,6 +47,9 @@ List<String> env(services, names) {
     List<String> out = []
     if (services.contains('mongodb')) {
         out.add("MONGODB_HOST=${names[services.indexOf('mongodb')]}")
+    }
+    if (services.contains('mysql')) {
+        out.add("MYSQL_HOST=${names[services.indexOf('mysql')]}")
     }
     return out
 }
@@ -90,6 +93,52 @@ spec:
       - emptyDir: {}
         name: data
 """
+        case 'mysql':
+        return """
+apiVersion: v1
+kind: DeploymentConfig
+metadata:
+  name: ${name}
+  labels:
+    name: ${name}
+spec:
+  replicas: 0
+  selector:
+    name: ${name}
+  strategy:
+    recreateParams:
+      timeoutSeconds: 600
+    type: Recreate
+  template:
+    metadata:
+      labels:
+        name: ${name}
+    spec:
+      containers:
+      - env:
+        - name: MYSQL_USER
+          value: mysql
+        - name: MYSQL_PASSWORD
+          value: password
+        - name: MYSQL_ROOT_PASSWORD
+          value: password
+        - name: MYSQL_DATABASE
+          value: sampledb
+        image: registry.access.redhat.com/rhscl/mysql-57-rhel7@sha256:b8b26b5201adafb028757d7e574d377fe3e5ce5de019d783261352de2c58fcb1
+        imagePullPolicy: IfNotPresent
+        name: ${name}
+        ports:
+        - containerPort: 3306
+          protocol: TCP
+        volumeMounts:
+        - mountPath: /var/lib/mysql/data
+          name: mysql-data
+      dnsPolicy: ClusterFirst
+      restartPolicy: Always
+      volumes:
+      - emptyDir: {}
+        name: mysql-data
+"""
         default:
         return ''
     }
@@ -110,6 +159,23 @@ spec:
   - port: 27017
     protocol: TCP
     targetPort: 27017
+  selector:
+    name: ${name}
+  type: ClusterIP
+"""
+        case 'mysql':
+        return """
+apiVersion: v1
+kind: Service
+metadata:
+  name: ${name}
+  labels:
+    name: ${name}
+spec:
+  ports:
+  - port: 3306
+    protocol: TCP
+    targetPort: 3306
   selector:
     name: ${name}
   type: ClusterIP
